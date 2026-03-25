ecab884588f0c1fe1af1415c8e7941e86a74a506
Eduardo Ferraz <edupferraz@gmail.com>
2026-03-25 17:57:14 -0300

    feat: implement user search functionality and user finder composer

--- src/main/routes/users_routes.py
+++ src/main/routes/users_routes.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
+from src.main.composer.user_finder_composer import user_finder_composer
+from src.views.http_types.http_request import HttpRequest
 
 users_routes = APIRouter(tags=["Users"])
 
@@ -9,4 +11,15 @@ async def create_user():
   return JSONResponse(
     content={"Hello": "World"},
     status_code=200
+  )
+  
+@users_routes.get("/users/{user_name}")
+async def search_users_by_name(user_name: int):
+  http_request = HttpRequest(path_params={"user_name": user_name})
+  user_finder = user_finder_composer()
+  
+  http_response = await user_finder.handle_find_user_by_name(http_request)
+  return JSONResponse(
+    content=http_response.body,
+    status_code=http_response.status_code
   )
